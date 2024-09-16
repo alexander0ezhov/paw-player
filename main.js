96203b96e0f5679081200fe7c7842d8122d29d7d
alexander0ezhov <alexander0ezhov@gmail.com>
2024-09-17 02:10:24 +0300

    configured client side

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-    win.loadFile('./public/index.html')
-}
+  process.env.NODE_ENV === "development"
+    ? win.loadURL("http://localhost:3000/")
+    : win.loadFile("./client/dist/index.html");
+};
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
