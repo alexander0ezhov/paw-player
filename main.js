bfc52a1ee4f0aba9a59345fa7dc90aac9abea76e
alexander0ezhov <alexander0ezhov@gmail.com>
2024-09-18 01:32:27 +0300

    added some scripts + forge

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+const isDev = process.env.NODE_ENV === "development";
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    devTools: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  process.env.NODE_ENV === "development"
+  isDev
     ? win.loadURL("http://localhost:3000/")
     : win.loadFile("./client/dist/index.html");
 };
