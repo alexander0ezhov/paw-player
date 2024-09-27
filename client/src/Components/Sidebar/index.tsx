d0a3574fb3761ded9f3a1f28591285d432401efe
alexander0ezhov <alexander0ezhov@gmail.com>
2024-09-28 02:01:17 +0300

    add container background

--- client/src/Components/Sidebar/index.tsx
+++ client/src/Components/Sidebar/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { IComponentProps } from "@root/global-types";
 import SidebarHeader from "@Components/Sidebar/elem/SidebarHeader";
 import cn from "classnames";
+import globalcss from "@root/global.module.scss";
 import s from "./index.module.scss";
 
 const Sidebar: React.FC<IComponentProps> = ({ className }) => {
   return (
-    <aside className={cn(s.root, className)}>
+    <aside className={cn(s.root, globalcss.container, className)}>
       <SidebarHeader toggleSize={() => {}} />
     </aside>
   );
