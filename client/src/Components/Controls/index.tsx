29a55af7481232df20563b8f178297b73588ebc3
alexander0ezhov <alexander0ezhov@gmail.com>
2024-09-28 22:26:01 +0300

    container border

--- client/src/Components/Controls/index.tsx
+++ client/src/Components/Controls/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import cn from "classnames";
+import globalcss from "@root/global.module.scss";
 import { IComponentProps } from "@root/global-types";
 import s from "./index.module.scss";
 
 const Content: React.FC<IComponentProps> = ({ className }) => {
-  return <main className={cn(s.root, className)}>Controls</main>;
+  return (
+    <main className={cn(s.root, globalcss.container, className)}>Controls</main>
+  );
 };
 
 export default Content;
