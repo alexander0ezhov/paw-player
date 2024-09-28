19ae246c611f38fa1578d75c94ba672a15805a80
alexander0ezhov <alexander0ezhov@gmail.com>
2024-09-28 23:21:41 +0300

    sidebar expand/collapse

--- client/src/Components/Sidebar/index.tsx
+++ client/src/Components/Sidebar/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import { IComponentProps } from "@root/global-types";
-import SidebarHeader from "@Components/Sidebar/elem/SidebarHeader";
+import React, { RefObject, useCallback, useRef, useState } from "react";
 import cn from "classnames";
+import { IComponentProps } from "@root/global-types";
 import globalcss from "@root/global.module.scss";
+import useOnClickOutside from "@root/hooks/useOnClickOutside";
+import SidebarHeader from "@Components/Sidebar/elem/SidebarHeader";
 import s from "./index.module.scss";
 
 const Sidebar: React.FC<IComponentProps> = ({ className }) => {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef() as RefObject<HTMLElement>;
+  const toggleExpanded = useCallback(
+    () => setExpanded((prev) => !prev),
+    [setExpanded],
+  );
+
+  useOnClickOutside(ref, setExpanded.bind(null, false));
+
   return (
-    <aside className={cn(s.root, globalcss.container, className)}>
-      <SidebarHeader toggleSize={() => {}} />
+    <aside
+      className={cn(
+        s.root,
+        globalcss.container,
+        expanded && s.expanded,
+        className,
+      )}
+      ref={ref}
+    >
+      <SidebarHeader toggleSize={toggleExpanded} />
     </aside>
   );
 };
