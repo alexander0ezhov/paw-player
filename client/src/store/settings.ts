ee96100007a96044c400ff5fb18e4dc2e234b332
alexander0ezhov <alexander0ezhov@gmail.com>
2024-10-11 15:19:57 +0300

    init settings

--- client/src/store/settings.ts
+++ client/src/store/settings.ts
@@ -2,19 +2,31 @@ import { create } from "zustand";
 import defaultSettings from "@root/default-settings.json";
 import { ThemeModes, SettingsType } from "@root/global-types";
 import { setThemeModeToDOM } from "@utils/func";
-import { getSettings } from "@utils/localStorage";
+import {
+  getLocalStorageSettings,
+  setLocalStorageSettings,
+} from "@utils/localStorage";
 
-const initialSettings = (getSettings() || defaultSettings) as SettingsType;
+const getSettings = () => {
+  const localStorageSettings = getLocalStorageSettings();
+  if (!localStorageSettings) {
+    setLocalStorageSettings(defaultSettings as SettingsType);
+    return defaultSettings as SettingsType;
+  }
+  return localStorageSettings;
+};
+
+const settings = getSettings();
+setThemeModeToDOM(settings.themeMode);
 
 type State = SettingsType;
-
 type Actions = {
   setThemeMode: (themeMode: SettingsType["themeMode"]) => void;
   toggleThemeMode: () => void;
 };
 
 export const useSettingsStore = create<State & Actions>((set) => ({
-  ...initialSettings,
+  ...settings,
   setThemeMode: (themeMode) => set({ themeMode }),
   toggleThemeMode: () =>
     set((state) => {
--- client/src/utils/localStorage.ts
+++ client/src/utils/localStorage.ts
@@ -1,13 +1,13 @@
 import { SettingsType } from "@root/global-types";
 const settingsKey = "settings";
 
-export const getSettings = () => {
+export const getLocalStorageSettings = () => {
   const settings = localStorage.getItem(settingsKey);
   if (!settings) return null;
   return JSON.parse(settings) as SettingsType;
 };
 
-export const setSettings = (newSettings: SettingsType): void => {
+export const setLocalStorageSettings = (newSettings: SettingsType): void => {
   localStorage.setItem(settingsKey, JSON.stringify(newSettings));
 };
 
