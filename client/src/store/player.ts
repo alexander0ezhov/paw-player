8f4bac01cc56389b3b1024a797e8b32809ed8443
alexander0ezhov <alexander0ezhov@gmail.com>
2024-11-13 01:04:19 +0300

    player audio events

--- client/src/store/player.ts
+++ client/src/store/player.ts
@@ -8,27 +8,30 @@ type Actions = {
   pause: () => void;
 };
 
-const audio = new Audio();
-
-export const usePlayerStore = create<State & Actions>((set, get) => ({
-  // audio,
-  isPlaying: false,
-  currentTrack: undefined,
-  setCurrentTrack: async (track) => {
-    if (!track.src) {
-      track.src = await window.node.readFileStream(track.path);
-      // error handling
-    }
-    audio.src = track.src || "";
-    set({ currentTrack: track });
-    get().play();
-    // TODO: should save track to config in node
-  },
-  play: () => {
-    audio.play().then(() => set({ isPlaying: true }));
-  },
-  pause: () => {
-    audio.pause();
-    set({ isPlaying: false });
-  },
-}));
+export const usePlayerStore = create<State & Actions>((set, get) => {
+  const audio = new Audio();
+  audio.onplay = () => set({ isPlaying: true });
+  audio.onpause = () => set({ isPlaying: false });
+  return {
+    // audio,
+    isPlaying: false,
+    currentTrack: undefined,
+    setCurrentTrack: async (track) => {
+      if (!track.src) {
+        track.src = await window.node.readFileStream(track.path);
+        // error handling
+      }
+      audio.src = track.src || "";
+      set({ currentTrack: track });
+      get().play();
+      // TODO: should save track to config in node
+    },
+    play: () => {
+      if (!get().currentTrack) return;
+      audio.play();
+    },
+    pause: () => {
+      audio.pause();
+    },
+  };
+});
